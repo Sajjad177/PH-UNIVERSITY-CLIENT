704fc0ffd4d5d4119b986e9930ce7ed23a25f2cc
sajjad <sajjadsajjad098765@gmail.com>
2025-01-11 14:21:40 +0600

    token verifyed successfully

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,16 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// module : 27-8 : redux-persist
+const perisitConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(perisitConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     // connect baseApi to store
     [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      // redux-persist
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
